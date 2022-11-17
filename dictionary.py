1a5220f495707299bb8d065b56d754562cc3674b
Facundo Perka <103785222+VNiten@users.noreply.github.com>
2022-11-17 17:49:22 -0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,14 @@
 numbers = {
-    "0": "Cero",
-    "1": "Uno",
-    "2": "Dos",
-    "3": "Tres",
-    "4": "Cuatro",
-    "5": "Cinco",
-    "6": "Seis",
-    "7": "Siete",
-    "8": "Ocho",
-    "9": "Nueve",
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
 }
 
 text = input('Write your number: ')
